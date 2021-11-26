f89bf98cbcde4af956a7f6760ff61b1496f7f5ae
FranCILDO Alves <53942335+francildoalves@users.noreply.github.com>
2021-11-25 21:59:47 -0300

    hjhj

--- src/br.com.dio.exercicios/loops/Ex02_Nota.java
+++ src/br.com.dio.exercicios/loops/Ex02_Nota.java
@@ -3,8 +3,14 @@ package br.com.dio.exercicios.loops;
 /*
 WHILE
 Nota: Faça um programa que peça uma nota, entre zero e dez.
-Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido.
+Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe 
+um valor válido.
  */
 
-public class Ex02_Nota {
+public class Ex02_Nota (int nota){
+    int nota = this.nota;
+    while (nota<0 && nota >10) {
+        System.out.println("");        
+    }
+
 }
