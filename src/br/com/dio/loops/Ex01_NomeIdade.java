5490dc7bca8e9b9a7499ea65767e1aa171179aee
FranCILDO Alves <53942335+francildoalves@users.noreply.github.com>
2021-12-02 16:53:37 -0300

    Iniciando o exercpicio1

--- src/br/com/dio/loops/Ex01_NomeIdade.java
+++ src/br/com/dio/loops/Ex01_NomeIdade.java
@@ -2,8 +2,26 @@ package br.com.dio.loops;
 
 /*
 WHILE
-Nome e Idade: Faça um programa que leia conjuntos de dois valores, o primeiro representando a matrícula do aluno
-e o segundo representando a sua altura em centímetros. (Pare inserindo o valor 0 no campo nome)
+Nome e Idade: Faça um programa que leia conjuntos de dois valores, o primeiro representando o nome do aluno
+e o segundo representando a sua idade. (Pare o programa inserindo o valor 0 no campo nome).
  */
 
-//public class Ex01_NomeIdade {}
+
+import javax.swing.JOptionPane;
+
+
+public class Ex01_NomeIdade {
+    public static void main(String[] args) {
+        //Scanner scn = new Scanner(System.in);
+        String nomeDoAluno = "";
+
+
+        while (nomeDoAluno != Integer.toString(0)) {
+            nomeDoAluno =JOptionPane.showInputDialog("Digite o nome do aluno ou ZERO para sair");
+            int idadeDoAluno = Integer.parseInt(JOptionPane.showInputDialog("Digite a idade do aluno:"));
+            //JOptionPane.showMessageDialog(null, "O nome do aluno é: " + nomeDoAluno + " a idade é " + idadeDoAluno);
+        }
+
+    }
+
+}
