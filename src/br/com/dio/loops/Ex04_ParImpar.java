db7c3638785cd3fd0d56209b6060399bca42b133
FranCILDO Alves <53942335+francildoalves@users.noreply.github.com>
2021-12-10 15:05:41 -0300

    Ex4
    Iniciando o Ex 04

--- src/br/com/dio/loops/Ex04_ParImpar.java
+++ src/br/com/dio/loops/Ex04_ParImpar.java
@@ -6,4 +6,26 @@ Par e Ímpar: Faça um programa que peça N números inteiros. Calcule e mostre 
 pares e a quantidade de números impares.
  */
 
-//public class Ex04_ParImpar {}
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Ex04_ParImpar {
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+
+        try {
+            boolean continuar = true;
+            int contador = 0;
+            int vetor[] = new int[contador];
+            do {
+                System.out.println("Digite um número: ");
+                vetor[contador] = leia.nextInt();
+                contador++;
+            } while (continuar);
+        } catch (NumberFormatException ne) {
+            System.out.println("Informe um número inteiro" + ne.getMessage());
+        } catch (Exception e) {
+            System.out.println("ERRO: " + e.getMessage());
+        }//Fim do catch
+    }//Fim do Main
+}//Fim da classe
